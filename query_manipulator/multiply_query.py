8d6ca95c8759d33fdd7ed238c70f17f128e188a9
Salvador Ludovico <49698940+salvadorludovico@users.noreply.github.com>
2024-10-28 10:08:34 -0300

    fix: multiply query

--- query_manipulator/multiply_query.py
+++ query_manipulator/multiply_query.py
@@ -1,17 +1,36 @@
+import json
+import re
+
 def multiply_query(llm_instance, query, n):
     prompt = f"""
         Você é um assistente especializado em gerar perguntas relacionadas a um determinado tema.
-        Dada uma pergunta, gere exatamente {n} perguntas relevantes e relacionadas ao mesmo tema.
+        Dada uma pergunta do usuário, gere exatamente {n} perguntas relevantes e relacionadas ao mesmo tema.
         Mantenha o foco no mesmo contexto, explorando ângulos diferentes.
-        Retorne estritamente cada pergunta em uma nova linha, sem numeração, sem símbolos, sem preâmbulos, títulos ou qualquer outro texto adicional. 
+        Retorne sem preâmbulos, títulos ou qualquer outro texto adicional.
+        Retorne apenas um json com a lista de perguntas geradas no seguinte formato:
+        
+        ```json
+            "perguntas": ["pergunta 1", "pergunta 2", "...", "pergunta n"]
+        ```
 
         PERGUNTA:
         {query}
     """
-
     
     result = llm_instance.invoke(prompt)
 
-    generated_queries = result.strip().split("\n")
+    result = result[7:-4].strip()
+
     
-    return [query] + generated_queries
+    if result:
+        try:
+            data = json.loads(result)
+            perguntas = data.get("perguntas", [])
+        except json.JSONDecodeError:
+            print("Erro ao decodificar JSON")
+            perguntas = []
+    else:
+        print("JSON não encontrado na resposta")
+        perguntas = []
+    
+    return [query] + perguntas
